32f94f5c542cc3e2ab7aee38af481db8906edfa1
jdoteduardo <joseeduardo0131@gmail.com>
2022-11-09 01:35:36 +0000

    arquivos iac

---
+++ iac1.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "Criando diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+
+useradd carlos -m -s /bin/bash -G GRP_ADM
+useradd maria -m -s /bin/bash -G GRP_ADM
+useradd joao -m -s /bin/bash -G GRP_ADM
+
+useradd debora -m -s /bin/bash -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -G GRP_VEN
+useradd roberta -m -s /bin/bash -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -G GRP_SEC
+useradd amanda -m -s /bin/bash -G GRP_SEC
+useradd rogerio -m -s /bin/bash -G GRP_SEC
+
+passwd carlos 123
+passwd maria 123
+passwd joao 123
+passwd debora 123
+passwd sebastiana 123
+passwd roberta 123
+passwd josefina 123
+passwd amanda 123
+passwd rogerio 123
+
+echo "Especificando permissões dos diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Fim..."
+
+
+
+
